bf2031f9756afa9b44f37cd3a6cbf97e4b65e1d7
ROUNAK BHATTACHARYA <72132759+Ron7978@users.noreply.github.com>
2022-11-04 15:17:29 +0530

    Assignment

---
+++ 01.11.22 Assignment/Q1/Q1_Event/Q1_Event/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Q1_Event
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Console.WriteLine("Hello, World!");
+                string connection = "Data Source=INLPF37B1HY\\MSSQLSERVER1;Initial Catalog=event;trusted_connection=true";
+                SqlConnection sqlcon = new SqlConnection(connection);
+                sqlcon.Open();
+                Console.WriteLine("Enter the month to fetch all the events happening in that month:   ");
+                string input = Console.ReadLine();
+                SqlCommand cmd = new SqlCommand("event_info", sqlcon);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@input_month", input);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader.GetString(0));
+                }
+                reader.Close();
+                // cmd.ExecuteNonQuery();
+                sqlcon.Close();
+
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("this is sql exception:    " + e.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("This is exception in c#:    " + ex.Message);
+            }
+        }
+    }
+}
